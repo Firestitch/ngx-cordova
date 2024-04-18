ddd520539003f48b2a6956f03935fcdcadbf6b1f
Raymond Gigliotti <ray@firestitch.com>
2024-04-18 12:37:53 -0400

    added FsCapacitorHttpndefined

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,4 +1,5 @@
-export * from './cordova.service';
+export * from './capacitor-http.service';
+export * from './cordova-camera-file.service';
 export * from './cordova-cookie.service';
 export * from './cordova-http.service';
-export * from './cordova-camera-file.service';
+export * from './cordova.service';
